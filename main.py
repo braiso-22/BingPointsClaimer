896f599d3a17dceeea701ebc489515ab69e0885f
braiso_22 <braisfv22@gmail.com>
2022-07-19 23:25:58 +0200

    Login and accept cookies

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import pyautogui
 
-
 browserOptions = Options()
 browserOptions.add_argument("start-maximized")
+# TODO change to your path
+browserOptions.add_argument("--user-data-dir= ??????")
+browserOptions.add_argument('--profile-directory=Default')
 
 browser = webdriver.Edge(service=Service(
     "./driver/msedgedriver.exe"), options=browserOptions)
 
 browser.get("https://bing.com/")
 
+cookiesAcceptButton = WebUi(browser, 20).until(
+    EC.element_to_be_clickable((By.ID, "bnp_btn_accept")))
+
+cookiesAcceptButton.click()
