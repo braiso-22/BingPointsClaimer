67e705d23ab8bd47e77207c1e6c04e4b19a3a0e2
braiso_22 <braisfv22@gmail.com>
2022-07-20 22:28:05 +0200

    add daily quizes search

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait as WebUi
+from selenium.webdriver.support.ui import WebDriverWait as uiSearcher
 from selenium.webdriver.support import expected_conditions as EC
 
 import sys
+import time
 
 browserOptions = Options()
 
@@ -16,12 +17,13 @@ def main():
     initOptions()
     initBrowser()
     dailySearches()
+    dailyQuizes()
 
     print("Done")
 
 
 def initOptions():
-    browserOptions.add_argument("start-maximized")
+    # browserOptions.add_argument("start-maximized")
 
     userPath = "--user-data-dir=" + str(sys.argv[1])
     browserOptions.add_argument(userPath)
@@ -33,12 +35,52 @@ def initBrowser():
     global browser
     browser = webdriver.Edge(service=Service(
         "./driver/msedgedriver.exe"), options=browserOptions)
+    goToBing()
+
+
+def goToBing():
     browser.get("https://bing.com/")
 
 
 def dailySearches():
     for i in range(0, 34):
         browser.get("https://www.bing.com/search?q=" + str(i))
+
+
+def dailyQuizes():
+    goToBing()
+    callQuizes()
+    allQuizes = getAllQuizes()
+    for quiz in allQuizes:
+        print(quiz)
+
+
+def callQuizes():
+    rewardsButton = uiSearcher(browser, 40).until(
+        EC.element_to_be_clickable((By.ID, "id_rc")))
+    time.sleep(1)
+    rewardsButton.click()
+
+
+def getAllQuizes():
+    arrayOfQuizes = list()
+    innerHtmlContainer = browser.find_element(By.ID, value="bepfm")
+
+    browser.switch_to.frame(innerHtmlContainer)
+
+    firstList = browser.find_elements(By.XPATH, "//div[@class='promo_cont']/a")
+    for link in firstList:
+        arrayOfQuizes.append(link.get_attribute("href"))
+
+    uiSearcher(browser, 40).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "chevronUp"))).click()
+
+    secondList = browser.find_elements(
+        By.XPATH, "//div[@class='promo_cont']/a")
+    for link in secondList:
+        arrayOfQuizes.append(link.get_attribute("href"))
+
+    return arrayOfQuizes
 
 
 if __name__ == "__main__":
